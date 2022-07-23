a286f80cf1e2a7082c42078a9a3916a2c1af8b39
realhardik18 <realhardik18@gmail.com>
2022-07-23 11:29:03 +0530

    testing some things out

--- endpoints.py
+++ endpoints.py
@@ -1,10 +1,20 @@
 from opensky_api import OpenSkyApi
 import random
-api = OpenSkyApi()
+import ast
 
+api = OpenSkyApi()
 states = api.get_states()
-'''
-flight_choosen = states.states[random.randint(0, len(states.states))]
-flight_choosen_str = str(flight_choosen)
-'''
-print(states.states[0])
+
+
+def available_states():
+    return len(states.states)
+
+
+def info():
+    flight_choosen = states.states[random.randint(0, len(states.states))]
+    flight_choosen_str = str(flight_choosen)
+    result = ast.literal_eval(flight_choosen_str)
+    return result
+
+
+# print(available_states())
